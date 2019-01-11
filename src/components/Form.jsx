71ecb48edd7d50e8bdee53711219f31d07bf66d4
dimola7.github.io <didiogidan@gmail.com>
2019-01-11 15:54:13 +0100

    removed .env from .gitignore

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -25,7 +25,7 @@ class Form extends Component {
     axios({
       method: 'post',
       url: `https://api.airtable.com/v0/app0s53GIQZBB0T5d/Cuisines`,
-      headers: {Authorization: `Bearer keyhAAqvSVG6kZeVZ`},
+      headers: { Authorization: `Bearer keyhAAqvSVG6kZeVZ` },
       data: {
         fields: {
           Name: this.state.mealName,
