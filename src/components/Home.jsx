0aa8d03d02064c008cf2e96eaa0df99f7e70499e
dimola7.github.io <didiogidan@gmail.com>
2019-03-01 14:33:39 +0100

    made image the link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -95,7 +95,7 @@ const HomeStyle = styled.div`
  }
 }
 .img-btn{
-  opacity: 0;
+  // opacity: 0;
   position: relative;
   bottom: 320px;
   height: 82%;
@@ -104,7 +104,13 @@ const HomeStyle = styled.div`
   border: none;
 
   @media(max-width: 720px){
-    height: 280px;
+    // opacity: 0;
+    position: relative;
+    bottom: 180px;
+    height: 60px;
+    width: 100%;
+    cursor: pointer;
+    border: none;
   }
     
 }
